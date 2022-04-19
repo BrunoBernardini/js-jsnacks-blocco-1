22a8b8f0219b7c51f6560a2965486ee56655585a
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-04-19 02:44:17 +0200

    JSnack7.

--- jsnack7/assets/JS/script.js
+++ jsnack7/assets/JS/script.js
@@ -1,24 +1,19 @@
 /*
-  JSnack 2
-  L’utente inserisce due parole in successione, con due prompt.
-  Il software stampa prima la parola più corta, poi la parola più lunga.
+  JSnack 7
+  Stampa le potenze di 2 fino a 1000.
+
+  questo snack cela un trabocchetto:  1000 non è una potenza di 2 perciò per fermarsi prima di 1000 bisogna stampare la potenza prima di incrementarla perché altrimenti il controllo viene fatto dopo e si sfora
 */
 
-const firstWord = prompt("Inserisci la prima parola");
-const secWord = prompt("Inserisci la seconda parola");
+const limit = 1000;
+let number = 2;
 
 const output = document.querySelector(".wrapper");
+let result = `<h1>Le potenze di ${number} fino a ${limit}:</h1>
+              <ul>`;
 
-let biggerWord, smallerWord;
-
-if(firstWord.length >= secWord.length){
-  biggerWord = firstWord;
-  smallerWord = secWord;
-}
-else{
-  biggerWord = secWord;
-  smallerWord = firstWord;
+for(let i=0; Math.pow(number,i)<limit; i++){
+  result += `<li>${Math.pow(number,i)}</li>`;
 }
 
-output.innerHTML = `<h2>Parole inserite:</h2>
-                    <h1><span class="first-word">${biggerWord}</span> - <span class="second-word">${smallerWord}</span></h1>`;
+output.innerHTML = result + "</ul>";
